362b33b4784ede3cceb44e3ce82a4b841aba41b1
Aqsa Abbasi <aqsaabbasi2690@gmail.com>
2025-07-04 20:08:54 +0500

    Update calculator.py

--- Utility Tools Section/calculator.py
+++ Utility Tools Section/calculator.py
@@ -1,3 +1,11 @@
+import os
+if not os.path.exists("data/logged_in.txt"):
+    print("Access Denied. Please log in first.")
+    exit()
+
+
+
+
 num1 = int( input("Enter first number:"))
 num2 = int(input("Enter second number:"))
 print("num1 + num2:",num1 + num2)
