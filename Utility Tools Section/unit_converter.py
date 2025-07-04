7dc2515a7c823295640b698d4b12eabab76ea8e4
Aqsa Abbasi <aqsaabbasi2690@gmail.com>
2025-07-04 18:09:56 +0500

    Update unit_converter.py

--- Utility Tools Section/unit_converter.py
+++ Utility Tools Section/unit_converter.py
@@ -1,31 +1,31 @@
-unit = input("Which unit you want to convert:(Weight / Length / Temperature)").lower
-
+unit = input("Which unit you want to convert? (Weight / Length / Temperature): ").lower()
 
 if unit == "length":
-    val = float(input("Enter value:"))
-    print("1 Meters to Feet\n2 Feet to Meters")
-    option = input("Choose one option:")
+    val = float(input("Enter value: "))
+    print("1. Meters to Feet\n2. Feet to Meters")
+    option = input("Choose one option: ")
     if option == "1":
-        print(f"{val} meters = {val * 3.281:.2f}feet")
+        print(f"{val} meters = {val * 3.281:.2f} feet")
     else:
-        print(f"{val} feet = {val / 3.281:.2f}meters")
+        print(f"{val} feet = {val / 3.281:.2f} meters")
 
 elif unit == "weight":
-   val = float(input("Enter value:"))
-   print("1 KG to Pounds\n2 Pounds to KG")
-   option = input("Choose one option:")
-   if option== "1":
-        print(f"{val} kg = {val * 2.20462:.2f}pounds")
-   else:
-        print(f"{val} pounds = {val / 3.281:.2f}kg")
+    val = float(input("Enter value: "))
+    print("1. KG to Pounds\n2. Pounds to KG")
+    option = input("Choose one option: ")
+    if option == "1":
+        print(f"{val} kg = {val * 2.20462:.2f} pounds")
+    else:
+        print(f"{val} pounds = {val / 2.20462:.2f} kg")
 
-elif unit == "temp":
-      val = float(input("Enter temperature:"))
-      print("1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius")
-      option = input("Choose one option:")
-      if option  == "1":
-          print(f"{val}°C = {(val * 9/5) + 32:.2f}°F")
-      else:
-       print(f"{val}°F = {(val - 32) * 5/9:.2f}°C")
+elif unit == "temperature":
+    val = float(input("Enter temperature: "))
+    print("1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius")
+    option = input("Choose one option: ")
+    if option == "1":
+        print(f"{val}°C = {(val * 9/5) + 32:.2f}°F")
+    else:
+        print(f"{val}°F = {(val - 32) * 5/9:.2f}°C")
+
 else:
-    ("invaid option.")
+    print("Invalid option.")
